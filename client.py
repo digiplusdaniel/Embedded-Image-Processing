8e4a56444dda12187f00118036bebc269126d7c5
Daniel SY Huang <32094331+digiplusdaniel@users.noreply.github.com>
2018-03-27 21:24:55 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,29 @@
+# -*- coding: cp950 -*-
+import socket
+
+# host = socket.gethostname()
+# port = 5000
+host = '192.168.8.100'  # 對server端為主機位置
+port = 5555
+address = (host, port)
+
+socket02 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF_INET:默認IPv4, SOCK_STREAM:TCP
+
+socket02.connect(address)  # 用來請求連接遠程服務器
+
+##################################
+# 開始傳輸
+print('start send image')
+imgFile = open("file.png", "rb")
+while True:
+    imgData = imgFile.readline(512)
+    if not imgData:
+        break  # 讀完檔案結束迴圈
+    socket02.send(imgData)
+imgFile.close()
+print('transmit end')
+##################################
+
+socket02.close()  # 關閉
+print('client close')

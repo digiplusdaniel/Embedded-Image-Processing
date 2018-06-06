d38f638c851d9125826cd64fd273ab4ba37177de
William FJ Yang <37613436+WilliamFJYang@users.noreply.github.com>
2018-06-06 14:20:18 +0800

    MQTT_funtion

---
+++ MQTT_publish.py
@@ -0,0 +1,16 @@
+# import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import time
+
+HOST = "127.0.0.1"
+PORT = 61613
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("image")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+msg.payload.decode("utf-8"))
+
+if __name__ == '__main__':
+    client_id = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
+    publish.single("image", "photo", qos = 1,hostname=HOST,port=PORT, client_id=client_id,auth = {'username':"admin", 'password':"123456"})
